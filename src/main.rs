d11b95ddfb89341c4b9b8bc20e3e96d3d9024cb1
hahazexia <hahazexia@outlook.com>
2026-04-13 20:56:41 +0800

    find cl

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use clap::Parser;
+use std::env::home_dir;
+use std::mem;
+use windows::Win32::UI::Controls::Dialogs::{
+    GetOpenFileNameW, OFN_FILEMUSTEXIST, OFN_PATHMUSTEXIST, OPENFILENAMEW,
+};
+use windows::core::PWSTR;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    find_cl: bool,
+
+    #[arg(short, long)]
+    show_cl: bool,
+
+    #[arg(short, long)]
+    add_project: Option<String>,
+
+    #[arg(short, long)]
+    path_to_project: Option<String>,
+
+    #[arg(short, long)]
+    del_project: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if cli.find_cl {
+        let mut file_buf = [0u16; 260];
+
+        let mut ofn = OPENFILENAMEW {
+            lStructSize: mem::size_of::<OPENFILENAMEW>() as u32,
+            lpstrFile: PWSTR(file_buf.as_mut_ptr()),
+            nMaxFile: file_buf.len() as u32,
+            Flags: OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST,
+            ..Default::default()
+        };
+        let str = unsafe {
+            if GetOpenFileNameW(&mut ofn).as_bool() {
+                let len = file_buf
+                    .iter()
+                    .position(|&c| c == 0)
+                    .unwrap_or(file_buf.len());
+                Some(String::from_utf16_lossy(&file_buf[..len]))
+            } else {
+                None
+            }
+        };
+
+        let s = str.unwrap_or_else(|| "".to_string());
+
+        println!("file path is {s}");
+
+        if s.as_str().ends_with("vcvarsall.bat") {
+        } else {
+        }
+    }
+}

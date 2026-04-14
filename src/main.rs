d7d21c5ee0005f488dce4044b131fad3a38d078b
hahazexia <hahazexia@outlook.com>
2026-04-14 16:54:59 +0800

    find cl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use clap::Parser;
+use serde::{Deserialize, Serialize};
 use std::env::home_dir;
-use std::fs::create_dir_all;
+use std::fs::{self, create_dir_all};
 use std::mem;
 use windows::Win32::UI::Controls::Dialogs::{
     GetOpenFileNameW, OFN_FILEMUSTEXIST, OFN_PATHMUSTEXIST, OPENFILENAMEW,
 };
 use windows::core::PWSTR;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Config {
+    pub vcvars_path: String,
+    pub default_arch: String,
+    pub projects: Vec<Project>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Project {
+    pub name: String,
+    pub path: String,
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -26,7 +40,7 @@ struct Cli {
     del_project: bool,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     if cli.find_cl {
@@ -51,14 +65,32 @@ fn main() -> std::io::Result<()> {
             }
         };
 
-        let s = str.unwrap_or_else(|| "".to_string());
+        let cl_str = str.unwrap_or_else(|| "".to_string());
 
-        println!("file path is {s}");
+        println!("file path is {cl_str}");
 
-        if s.as_str().ends_with("vcvarsall.bat") {
+        if cl_str.as_str().ends_with("vcvarsall.bat") {
             if let Some(home) = home_dir() {
                 let config_file_dir = home.join(".ezcli");
-                let config_file = config_file_dir.join("/ezcli_config.json");
+                let config_file = config_file_dir.join("/ezcli.toml");
+
+                if !config_file_dir.exists() {
+                    create_dir_all(config_file_dir)?;
+                }
+                if !config_file.exists() {
+                    let default_config = Config {
+                        vcvars_path: cl_str,
+                        default_arch: "x64".to_string(),
+                        projects: vec![],
+                    };
+
+                    let toml_content = toml::to_string_pretty(&default_config)?;
+
+                    fs::write(&config_file, toml_content)?;
+
+                    println!("already create new ezcli.toml!");
+                } else {
+                }
             } else {
                 println!("get home dir failed!");
             }
